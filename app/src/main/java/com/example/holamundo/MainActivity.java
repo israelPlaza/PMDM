3a5ec2654802620bab955cd68378aedcd3e8907b
israelPlaza <israel.plaza.mayoral@gmail.com>
2024-09-24 18:25:21 +0200

    lo de ayer

---
+++ app/src/main/java/com/example/holamundo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.holamundo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+ int contador;
+ TextView contadorDeClics;
+ Button botonClick = findViewById(R.id.botonClick);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+            contadorDeClics= findViewById(R.id.contadorDeClics);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
